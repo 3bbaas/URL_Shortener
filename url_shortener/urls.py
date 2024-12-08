f8177e17942d43337bff92e0e51441c07cad2901
Ahmed Abbas <ah.abbas333@gmail.com>
2024-12-08 22:45:06 +0200

    edit paths

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.generate_url, name="home"),
 ]
+urlpatterns += staticfiles_urlpatterns()
